cfaf7f97665f6280991b994e54983b4b1c7cb42f
Andrew Donovan <a.o.donovan.music@gmail.com>
2024-05-25 02:56:02 -0400

    make unavailable time slots unclickable. extract tailwind class builder function.

--- src/app/ui/Pickup.js
+++ src/app/ui/Pickup.js
@@ -9,16 +9,18 @@ export default function Pickup({dispatch, existingInfo, warnings}){
 
   const [startDate, setStartDate] = useState(existingInfo.pickupDetails?.date ? new Date(existingInfo.pickupDetails.date) : '');
   const [time, setTime] = useState(existingInfo.pickupDetails?.pickupTime ? existingInfo.pickupDetails.pickupTime : null);
-  const [cakeDays, setCakeDays] = useState('');
+  const [cakeDays, setCakeDays] = useState([]);
+  const [unavailable, setUnavilable] = useState([]);
+  
 
   useEffect(() => {
     (async() => {
       const { cakeDays } = await getCakeDays();
-      setCakeDays(cakeDays.rows.filter(r => !r.available).map(r => new Date(r.date)));
+      setUnavilable(cakeDays.rows.filter(r => !r.available).map(r => new Date(r.date)));
+      setCakeDays(cakeDays.rows);
     })();
   }, [])
 
-  console.log(cakeDays)
   const isOffDay = (date) => {
     const day = getDay(date);
     return day === 5 || day === 6;
@@ -62,6 +64,24 @@ export default function Pickup({dispatch, existingInfo, warnings}){
     dispatch({type: "prev"});
   }
 
+  function isUnavilable(btnTime) {
+    if(!cakeDays.length){
+      return false;
+    } else {
+      return cakeDays.find(c => c.date === startDate)?.pickup1 === startDate
+    }
+  }
+
+  function buttonClasses(btnTime){
+    if(isUnavilable(btnTime)){
+      return "btn-disabled";
+    } else if(time === btnTime){
+      return "formBtn btn-success";
+    } else {
+      return "btn";
+    }
+  }
+
   return (
     <div className="Pickup h-full flex flex-col py-6">
       <h1 className="text-xl font-bold self-center">Pickup Info</h1>
@@ -72,21 +92,25 @@ export default function Pickup({dispatch, existingInfo, warnings}){
       <div className="flex gap-4 justify-self-start">
         <DatePicker
           selected={startDate}
-          onChange={(date) => setStartDate(date)}
+          onChange={(date) => {
+            setTime(null);
+            setStartDate(date);
+            }
+          }
           filterDate={isOffDay}
-          excludeDates={cakeDays}
+          excludeDates={unavailable}
           minDate={new Date()}
           placeholderText="Select a day for pickup"
           form="external-form"
           inline
         />
         <div className="flex flex-col justify-center items-center">
-          <label htmlFor="12pm" className={time === '12:00' ? "formBtn btn-success" : "btn"}>12:00pm</label>
-          <label htmlFor="1230pm" className={time === '12:30' ? "formBtn btn-success" : "btn"}>12:30pm</label>
-          <label htmlFor="1pm" className={time === '1:00' ? "formBtn btn-success" : "btn"}>1:00pm</label>
-          <label htmlFor="130pm" className={time === '1:30' ? "formBtn btn-success" : "btn"}>1:30pm</label>
-          <label htmlFor="2pm" className={time === '2:00' ? "formBtn btn-success" : "btn"}>2:00pm</label>
-          <label htmlFor="230pm" className={time === '2:30' ? "formBtn btn-success" : "btn"}>2:30pm</label>
+          <label htmlFor="12pm" className={buttonClasses('12:00')}>12:00pm</label>
+          <label htmlFor="1230pm" className={buttonClasses('12:30')}>12:30pm</label>
+          <label htmlFor="1pm" className={buttonClasses('1:00')}>1:00pm</label>
+          <label htmlFor="130pm" className={buttonClasses('1:30')}>1:30pm</label>
+          <label htmlFor="2pm" className={buttonClasses('2:00')}>2:00pm</label>
+          <label htmlFor="230pm" className={buttonClasses('2:30')}>2:30pm</label>
         </div>
       </div>
       <div className="flex justify-between justify-self-end items-end mt-8 h-full">
@@ -97,9 +121,9 @@ export default function Pickup({dispatch, existingInfo, warnings}){
           <input type="radio" name="pickupTime" id="12pm" value={'12:00'} onChange={timeClick} checked={time==='12:00'} hidden={true}/>
           <input type="radio" name="pickupTime" id="1230pm" value={'12:30'} onChange={timeClick} checked={time==='12:30'} hidden={true}/>
           <input type="radio" name="pickupTime" id="1pm" value={'1:00'} onChange={timeClick} checked={time==='1:00'} hidden={true}/>
-           <input type="radio" name="pickupTime" id="130pm" value={'1:30'} onChange={timeClick} checked={time==='1:30'} hidden={true}/>
-            <input type="radio" name="pickupTime" id="2pm" value={'2:00'} onChange={timeClick} checked={time==='2:00'} hidden={true}/>
-             <input type="radio" name="pickupTime" id="230pm" value={'2:30'} onChange={timeClick} checked={time==='2:30'} hidden={true}/>
+          <input type="radio" name="pickupTime" id="130pm" value={'1:30'} onChange={timeClick} checked={time==='1:30'} hidden={true}/>
+          <input type="radio" name="pickupTime" id="2pm" value={'2:00'} onChange={timeClick} checked={time==='2:00'} hidden={true}/>
+          <input type="radio" name="pickupTime" id="230pm" value={'2:30'} onChange={timeClick} checked={time==='2:30'} hidden={true}/>
           <input hidden={true} defaultValue={startDate} name="date"/>
           <button className="formBtn btn-primary justify-self-end w-[90px]">Next</button>
         </form>
