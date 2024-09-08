d9ae31bc9a6c80f1e9bd0e476aa863f9c75608fc
ao donovan <a.o.donovan.music@gmail.com>
2024-09-08 00:35:27 -0400

    add bg image to admin login card and replace plain button with login svg

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,13 +1,15 @@
 import { login } from "@/lib/actions";
+import Image from "next/image";
 
 export default function Page() {
 
   return (
-    <div className="card w-96 bg-neutral text-neutral-content flex flex-col justify-center self-center p-8">
+    <div className="card w-96 bg-neutral text-neutral-content flex flex-col justify-center self-center p-8" style={{backgroundImage: 'url(/jf-rip.png)', backgroundRepeat: 'repeat-y', backgroundSize: '300px 300px'}}>
       <div className="card-body items-center text-center">
         <form action={login} className="flex gap-8">
-          <input type="password" placeholder="" className="input input-ghost w-full max-w-xs bg-secondary" name="pwd" />
-          <button className="btn bg-primary w-1/4">X</button>
+          <input type="password" placeholder="" className="input w-full max-w-xs bg-primary" name="pwd" />
+          
+          <button className="btn w-1/4 btn-outline"><Image src={"/loginMinimal.svg"} height={70} width={70} alt="login" id="confirmed"/></button>
         </form>
       </div>
     </div>
