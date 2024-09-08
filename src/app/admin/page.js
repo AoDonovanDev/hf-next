5aadf36a89778e676c13c1ce31f5b0da84ba16c7
ao donovan <a.o.donovan.music@gmail.com>
2024-09-07 23:43:47 -0400

    delete placeholder txt on password input

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -6,7 +6,7 @@ export default function Page() {
     <div className="card w-96 bg-neutral text-neutral-content flex flex-col justify-center self-center p-8">
       <div className="card-body items-center text-center">
         <form action={login} className="flex gap-8">
-          <input type="password" placeholder="Type here" className="input input-ghost w-full max-w-xs bg-secondary" name="pwd" />
+          <input type="password" placeholder="" className="input input-ghost w-full max-w-xs bg-secondary" name="pwd" />
           <button className="btn bg-primary w-1/4">X</button>
         </form>
       </div>
