6aae71866a31ba9613f30f4a6bb34fca5a20714d
ao donovan <a.o.donovan.music@gmail.com>
2024-12-01 13:40:18 -0500

    capitalize first names in confirmation email

--- src/app/ui/Confirmation.js
+++ src/app/ui/Confirmation.js
@@ -17,12 +17,14 @@ export default function Confirmation ( { order } ) {
     return <button type="submit" className="formBtn btn-success" disabled={pending}>{pending ? <span className="loading loading-spinner text-primary"></span> : 'Send'}</button>;
   }
 
+  const capitalizedFirstName = order.first_name.split('').map((c, i) => i == 0 ? c.toUpperCase() : c).join('');
+
   return(
     <form className="flex flex-col justify-center w-full" action={confirmOrder}>
       <textarea placeholder="write something here beb"
                 className="textarea textarea-bordered textarea-lg w-full h-96 max-w-xs flex self-center mx-0 my-6 text-sm overflow-auto overscroll-contain"
                 name="emailBody"
-                defaultValue={`Hey ${order.first_name}!
+                defaultValue={`Hey ${capitalizedFirstName}!
                 
                 
 I take a 50% deposit within the week of order confirmation and the rest on pick up.The deposit can be made via Venmo to houseflyvictuals and the last half can be through Venmo or in cash on pick up. Please put a fun emoji as the tagline for your payment. The pick up address is 101 W Locust St and my phone number is 4233284070. Let me know if you have any questions!`}
